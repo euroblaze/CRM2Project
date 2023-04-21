cb03a216f8732000337dc5cc4622186f385c214e
Simplify-ERP® <github@euroblaze.de>
2023-04-21 08:55:57 +0200

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Plausibility Check Module",
+    "version": "1.0",
+    "category": "Sales",
+    "summary": "Module to perform plausibility checks on customer requirements",
+    "author": "Your Name",
+    "website": "https://www.example.com",
+    "depends": ["base", "sale", "project"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/plausibility_check_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
