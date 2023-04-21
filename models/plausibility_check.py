4fac0bca29a08b9aa102fdfb768edfff58d15fe4
Simplify-ERP® <github@euroblaze.de>
2023-04-21 08:55:06 +0200

    Create plausibility_check.py

---
+++ models/plausibility_check.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+
+class PlausibilityCheck(models.Model):
+    _name = "plausibility.check"
+    _description = "Plausibility Check for Customer Requirements"
+
+    customer_requirement_id = fields.Many2one("sale.order", "Customer Requirement")
+    state = fields.Selection(
+        [("draft", "Draft"), ("validated", "Validated"), ("invalid", "Invalid")],
+        "State",
+        default="draft",
+    )
+
+    @api.multi
+    def validate_requirement(self):
+        # Implement plausibility check logic here
+        self.state = "validated"  # or "invalid" if the check fails
